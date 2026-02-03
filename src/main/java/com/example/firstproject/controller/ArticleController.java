0aec62abf9b4e9103ec7bf8ab8e93f6d7402c13b
cccccJJh <lalalajhj@gmail.com>
2026-02-03 20:38:57 +0900

    강제 전체 업데이트

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,4 +1,26 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ArticleController {
+
+    @GetMapping("articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form){
+
+        System.out.println(form.toString());
+
+        //1. DTO를 엔티티로 변환
+        //2. 리파지터리로 엔티티를 DB에 저장
+        return "";
+
+    }
 }
